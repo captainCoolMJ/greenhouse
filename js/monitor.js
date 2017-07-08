b5a1bbd9078db3e1e399860fe1c2a069c9f28367
Faheem Akhtar <faheempiscean@gmail.com>
2017-07-08 08:45:48 +0400

    integrated internal temperature

--- js/monitor.js
+++ js/monitor.js
@@ -175,186 +175,221 @@ const dummyData = {
     ]
 }
 
-var greenhouseMonitor = weatherData => {
-    this.hours = weatherData.hours.map(item => util.getHour(item.time));
+class GreenhouseMonitor {
 
-    this.tempGraph = {
-        draw: node => {
-            const hoursData = [['Hours', 'External']].concat(weatherData.hours.map((item, index) => {
-                return [
-                    this.hours[index],
-                    item.temp
-                ]
-            }))
-            var data = google.visualization.arrayToDataTable(hoursData);
-
-            var options = {
-                curveType: 'function',
-                legend: { position: 'bottom' }
-            };
-
-            var chart = new google.visualization.LineChart(node);
-
-            chart.draw(data, options);
-        },
-        getValue: pos => {
-            return weatherData.hours.find(item => {
-                const hour = util.getHour(item.time)
-                return Math.round(pos) === hour
-            })
-        },
-        updateMark: value => {
-            $('#monitor-temp').closest('.item').find('mark').text(value)
-        }
-    }
-
-    this.sunlightGraph = {
-        draw: node => {
-            const sunriseHour = util.getHour(weatherData.sunrise)
-            const sunsetHour = util.getHour(weatherData.sunset)
-
-            const hoursData = [['Hours', 'Sunlight']].concat(weatherData.hours.map((item, index) => {
-                const hour = this.hours[index]
-                const isSunThere = (hour >= sunriseHour && hour <= sunsetHour) ? 1 : 0
-                return [
-                    hour,
-                    isSunThere
-                ]
-            }))
-            var data = google.visualization.arrayToDataTable(hoursData);
-
-            var options = {
-                title: 'Sunlight',
-                curveType: 'none',
-                legend: { position: 'bottom' }
-            };
-
-            var chart = new google.visualization.LineChart(node);
-
-            chart.draw(data, options);
-        },
-        getValue: pos => {
-            const sunriseHour = util.getHour(weatherData.sunrise)
-            const sunsetHour = util.getHour(weatherData.sunset)
-            return (pos >= sunriseHour && pos <= sunsetHour) ? 1 : 0
-        },
-        updateMark: value => {
-            $('#monitor-sunlight').closest('.item').find('mark').text(value ? 'ON' : 'OFF')
-        }
-    }
-
-    this.waterGraph = { 
-        draw: node => {
-            const precipitation = weatherData.pop
-
-            const hoursData = [['Hours', 'Raining']].concat(weatherData.hours.map((item, index) => {
-                return [
-                    this.hours[index],
-                    item.pop >= 80 ? 1 : 0
-                ]
-            }))
-            var data = google.visualization.arrayToDataTable(hoursData);
-
-            var options = {
-                title: 'Water',
-                curveType: 'none',
-                legend: { position: 'bottom' }
-            };
-
-            var chart = new google.visualization.LineChart(node);
-
-            chart.draw(data, options);
-        },
-        getValue: pos => {
-            return weatherData.hours.find(item => {
-                const hour = util.getHour(item.time)
-                return Math.round(pos) === hour
-            })
-        },
-        updateMark: value => {
-            $('#monitor-water').closest('.item').find('mark').text(value >= 80 ? 'ON' : 'OFF')
-        }
-    }
-
-    this.updateMark = percentage => {
-        let data
-        const pos = util.transformRanges(percentage, [0, 100], [0, 23])
-
-        data = this.tempGraph.getValue(pos)
-        this.tempGraph.updateMark(data.temp)
-
-        data = this.sunlightGraph.getValue(pos)
-        this.sunlightGraph.updateMark(data)
-
-        data = this.waterGraph.getValue(pos)
-        this.waterGraph.updateMark(data.pop)
-    }
-
-    this.slider = {
-        init: selector => {
-            const currentHour = util.getHour(weatherData.date)
-            const currentPos = util.transformRanges(currentHour, [0, 23], [0, 100])
-            this.updateMark(currentPos)
-            $(selector).slider({
-                value: currentPos,
-                slide: (event, ui) => {
-                    this.slider.onSliderChange(ui.value)
+    constructor(weatherData) {
+        this.weatherData = weatherData
+        this.internalData = {}
+        this.hours = weatherData.hours.map(item => util.getHour(item.time));
+        this.tempGraph = {
+            draw: node => {
+                let internalTemp = []
+                if(this.internalData.temperature) {
+                    internalTemp = this.internalData.temperature.reduce((prev, current) => {
+                        prev.push(current.val)
+                        return prev
+                    }, [])
                 }
-            });
-        },
-        onSliderChange: percentage => {
-            this.updateMark(percentage)
+                const hoursData = [['Hours', 'External', 'Internal']].concat(weatherData.hours.map((item, index) => {
+                    return [
+                        this.hours[index],
+                        item.temp,
+                        internalTemp[index] || 0
+                    ]
+                }))
+                var data = google.visualization.arrayToDataTable(hoursData);
+
+                var options = {
+                    curveType: 'function',
+                    legend: { position: 'bottom' }
+                };
+
+                var chart = new google.visualization.LineChart(node);
+
+                chart.draw(data, options);
+            },
+            getValue: pos => {
+                return weatherData.hours.find(item => {
+                    const hour = util.getHour(item.time)
+                    return Math.round(pos) === hour
+                })
+            },
+            updateMark: value => {
+                $('#monitor-temp').closest('.item').find('mark').text(value)
+            }
+        }
+        this.sunlightGraph = {
+            draw: node => {
+                const sunriseHour = util.getHour(weatherData.sunrise)
+                const sunsetHour = util.getHour(weatherData.sunset)
+
+                const hoursData = [['Hours', 'Sunlight']].concat(weatherData.hours.map((item, index) => {
+                    const hour = this.hours[index]
+                    const isSunThere = (hour >= sunriseHour && hour <= sunsetHour) ? 1 : 0
+                    return [
+                        hour,
+                        isSunThere
+                    ]
+                }))
+                var data = google.visualization.arrayToDataTable(hoursData);
+
+                var options = {
+                    title: 'Sunlight',
+                    curveType: 'none',
+                    legend: { position: 'bottom' }
+                };
+
+                var chart = new google.visualization.LineChart(node);
+
+                chart.draw(data, options);
+            },
+            getValue: pos => {
+                const sunriseHour = util.getHour(weatherData.sunrise)
+                const sunsetHour = util.getHour(weatherData.sunset)
+                return (pos >= sunriseHour && pos <= sunsetHour) ? 1 : 0
+            },
+            updateMark: value => {
+                $('#monitor-sunlight').closest('.item').find('mark').text(value ? 'ON' : 'OFF')
+            }
+        }
+        this.waterGraph = { 
+            draw: node => {
+                const precipitation = weatherData.pop
+
+                const hoursData = [['Hours', 'Raining']].concat(weatherData.hours.map((item, index) => {
+                    return [
+                        this.hours[index],
+                        item.pop >= 80 ? 1 : 0
+                    ]
+                }))
+                var data = google.visualization.arrayToDataTable(hoursData);
+
+                var options = {
+                    title: 'Water',
+                    curveType: 'none',
+                    legend: { position: 'bottom' }
+                };
+
+                var chart = new google.visualization.LineChart(node);
+
+                chart.draw(data, options);
+            },
+            getValue: pos => {
+                return weatherData.hours.find(item => {
+                    const hour = util.getHour(item.time)
+                    return Math.round(pos) === hour
+                })
+            },
+            updateMark: value => {
+                $('#monitor-water').closest('.item').find('mark').text(value >= 80 ? 'ON' : 'OFF')
+            }
+        }
+        this.updateMark = percentage => {
+            let data
+            const pos = util.transformRanges(percentage, [0, 100], [0, 23])
+
+            data = this.tempGraph.getValue(pos)
+            this.tempGraph.updateMark(data.temp)
+
+            data = this.sunlightGraph.getValue(pos)
+            this.sunlightGraph.updateMark(data)
+
+            data = this.waterGraph.getValue(pos)
+            this.waterGraph.updateMark(data.pop)
+        }
+        this.slider = {
+            init: selector => {
+                const currentHour = util.getHour(weatherData.date)
+                const currentPos = util.transformRanges(currentHour, [0, 23], [0, 100])
+                this.updateMark(currentPos)
+                $(selector).slider({
+                    value: currentPos,
+                    slide: (event, ui) => {
+                        this.slider.onSliderChange(ui.value)
+                    }
+                });
+            },
+            onSliderChange: percentage => {
+                this.updateMark(percentage)
+            }
+        }
+        this.renderer = {
+            temp: () => {
+                const $target = $('#monitor-temp')
+                $target.children().remove()
+                google.charts.setOnLoadCallback(() => {
+                    this.tempGraph.draw($target[0])
+                });
+            },
+            sunlight: () => {
+                const $target = $('#monitor-sunlight')
+                $target.children().remove()
+                google.charts.setOnLoadCallback(() => {
+                    this.sunlightGraph.draw($target[0])
+                });
+            },
+            water: () => {
+                const $target = $('#monitor-water')
+                $target.children().remove()
+                google.charts.setOnLoadCallback(() => {
+                    this.waterGraph.draw($target[0])
+                });
+            },
+            slider: () => {
+                this.slider.init('#slider')
+            }
         }
     }
 
-    this.renderer = {
-        temp: () => {
-            const $target = $('#monitor-temp')
-            $target.children().remove()
-            google.charts.setOnLoadCallback(() => {
-                this.tempGraph.draw($target[0])
-            });
-        },
-        sunlight: () => {
-            const $target = $('#monitor-sunlight')
-            $target.children().remove()
-            google.charts.setOnLoadCallback(() => {
-                this.sunlightGraph.draw($target[0])
-            });
-        },
-        water: () => {
-            const $target = $('#monitor-water')
-            $target.children().remove()
-            google.charts.setOnLoadCallback(() => {
-                this.waterGraph.draw($target[0])
-            });
-        },
-        slider: () => {
-            this.slider.init('#slider')
-        }
-    }
-    
-    const render = (parts = []) => {
+    render(parts = []) {
         parts.forEach(item => {
             this.renderer[item]()
         })
     }
 
-    google.charts.load('current', {'packages':['corechart']});
-    render(['temp', 'sunlight', 'water', 'slider'])
+    displayInternaldata({ temperature }) {
+        temperature = temperature.map(temp => {
+            temp.hour = util.getHour(temp.date)
+            return temp
+        })
+        temperature.sort((a, b) => {
+            if( a.hour === b.hour ) {
+                return 0
+            }
+            return a.hour < b.hour ? -1 : 1
+        })
+        this.internalData.temperature = temperature
+        this.render(['temp'])
+    }
+
+    init() {
+        google.charts.load('current', {'packages':['corechart']});
+        this.render(['temp', 'sunlight', 'water', 'slider'])
+    }
 }
 
 (function($) {
+    let greenhouseMonitor
     if(dummyData) {
-        greenhouseMonitor(dummyData)
+        greenhouseMonitor = new GreenhouseMonitor(dummyData)
+        greenhouseMonitor.init()
     } else {
         $.ajax({
             dataType: "json",
             url: apiEndPoints.weather,
             success: data => {
-                greenhouseMonitor(data)
+                greenhouseMonitor = new GreenhouseMonitor(data)
+                greenhouseMonitor.init()
             }
         })
     }
+
+    var iosocket = io.connect();
+
+    iosocket.on('connect', function () {
+        iosocket.on('schedule', function (res) {
+            greenhouseMonitor.displayInternaldata(res)
+        });
+    });
     
-})(jQuery)
+}(jQuery))
